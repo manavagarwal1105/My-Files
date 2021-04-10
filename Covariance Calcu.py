15bd08194b069f7ccd6e1358d16c154d15489894
manavagarwal1105 <70770873+manavagarwal1105@users.noreply.github.com>
2021-04-10 14:19:06 +0530

    Update Covariance Calcu.py

--- Covariance Calcu.py
+++ Covariance Calcu.py
@@ -3,25 +3,22 @@
 Created on Tue Apr  6 14:08:38 2021
 
 @author: Manav
- x_std = x_arr.std()
-    y_std = y_arr.std()
-    x_var = x_std**2
-    y_var = y_std**2
-    
+
 """
 
 import numpy as np
 
 def calc(x,y):
-    x_arr = np.array(x)
+    x_arr = np.array(x) #converting a list into an array
     y_arr = np.array(y)
-    x_mean = x_arr.mean()
+    x_mean = x_arr.mean() #Calculating the mean
     y_mean = y_arr.mean()
     n = len(x)
     
+    #Calculating the covariance
     cov = 0
     for i in range(len(x)):
-        cov += ((x_arr[i] - x_mean)*(y_arr[i] - y_mean))/n-1
+        cov += ((x_arr[i] - x_mean)*(y_arr[i] - y_mean))/n-1 
 
     return (x_mean,y_mean,cov)
 
